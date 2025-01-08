94e8a033576700af68a28752933634c4cd8cae97
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2025-01-08 08:56:32 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Instructions:
+// A store has a product inventory system. If a product is sold, it reduces the quantity in stock.
+// If the stock is zero, the system should indicate "Out of Stock".
+// Example:
+// Input: Initial stock = 5, Sold = 6
+// Output: Out of Stock
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int stock = 5;
+
+        Console.WriteLine("Enter the number of items sold:");
+        int sold = Convert.ToInt32(Console.ReadLine());
+
+        stock -= sold;
+
+        if (stock > 0)
+        {
+            Console.WriteLine($"Remaining stock: {stock}");
+        }
+        else
+        {
+            Console.WriteLine("Out of Stock");
+        }
+    }
+}
